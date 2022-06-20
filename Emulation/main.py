046aa18957326e9c1215b191f98a7df69c8121c3
Belzorium <boyer.florian07@gmail.com>
2022-06-21 01:30:48 +0200

    working version

--- Emulation/main.py
+++ Emulation/main.py
@@ -23,19 +23,16 @@ if not SML_File.exists:
 
 quiet = "--quiet" in sys.argv
 pyboy = PyBoy(SML_File.as_posix(), game_wrapper=True, window_type="headless" if quiet else "SDL2")
-pyboy.set_emulation_speed(1)
+pyboy.set_emulation_speed(0)
 sml = pyboy.game_wrapper()
 sml.start_game()
 
-debut = io.BytesIO()
-debut.seek(0)
+debut = open("./debut.save", "wb")
 pyboy.save_state(debut)
+debut.close()
 
 def step(genomes, config):
 	for genome_id, genome in genomes:
-		print(genome_id)
-		debut.seek(0)
-		pyboy.load_state(debut)
 		info = readLevelInfos()
 		genome.fitness = sml.fitness       
 		net = neat.nn.FeedForwardNetwork.create(genome, config)
@@ -63,6 +60,11 @@ def step(genomes, config):
 				stuckFrames += 1
 			else:
 				stuckFrames = 0
+		
+		debut = open("./debut.save", "rb")
+		pyboy.load_state(debut)
+		pyboy.tick()
+		debut.close()
 
 def readLevelInfos(): 
 	tiles = []
@@ -90,34 +92,35 @@ def sendInputs(manipulations):
 		pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
 
 def run(config_path):
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-        neat.DefaultSpeciesSet, neat.DefaultStagnation,
-        config_path)
-    config.genome_config.num_inputs = len(readLevelInfos()["tiles"])
+	config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+		neat.DefaultSpeciesSet, neat.DefaultStagnation,
+		config_path)
+	config.genome_config.num_inputs = len(readLevelInfos()["tiles"])
 
-    pop = neat.Population(config)
+	pop = neat.Population(config)
 
     # Add a stdout reporter to show progress in the terminal.
-    pop.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    pop.add_reporter(stats)
-    pop.add_reporter(neat.Checkpointer(5, filename_prefix="./checkpoints/neat-checkpoint-"))
+	pop.add_reporter(neat.StdOutReporter(True))
+	stats = neat.StatisticsReporter()
+	pop.add_reporter(stats)
+	pop.add_reporter(neat.Checkpointer(5, filename_prefix="./checkpoints/neat-checkpoint-"))
 
-    winner = pop.run(step,1000)
+	winner = pop.run(step)
 
     # Show output of the most fit genome against training data.
-    print('\nOutput:')
+	print('\nOutput:')
 
-    node_names = {0: 'A', 1: 'B', 2:'X', 3:'Y', 4:'UP', 5:'DOWN', 6:'LEFT', 7:'RIGHT'}
-    tiles = readLevelInfos()["tiles"]
-    for i in range(len(tiles)):
-        node_names[-len(tiles)+i] = "tile_"+str(i)
-    visualize.draw_net(config, winner, True, node_names=node_names)
-    visualize.plot_stats(stats, ylog=False, view=True)
-    visualize.plot_species(stats, view=True)
+	node_names = {0: 'A', 1: 'B', 2:'X', 3:'Y', 4:'UP', 5:'DOWN', 6:'LEFT', 7:'RIGHT'}
+	tiles = readLevelInfos()["tiles"]
+	for i in range(len(tiles)):
+		node_names[-len(tiles)+i] = "tile_"+str(i)
+	visualize.draw_net(config, winner, True, node_names=node_names)
+	visualize.plot_stats(stats, ylog=False, view=True)
+	visualize.plot_species(stats, view=True)
 
-    p = neat.Checkpointer.restore_checkpoint('./checkpoints/neat-checkpoint-4')
-    p.run(step, 10)
+	p = neat.Checkpointer.restore_checkpoint('./checkpoints/neat-checkpoint-4')
+	pyboy.set_emulation_speed(1)
+	p.run(step, 10)
 
 
 if __name__ == '__main__':
