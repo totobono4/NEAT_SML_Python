0f4abd5c4bcd9c8a6390e507787df0ba21be27b7
totobono4 <totobono4@outlook.fr>
2022-06-21 00:41:59 +0200

    ROMS+demo

--- Emulation/main.py
+++ Emulation/main.py
@@ -1,8 +1,16 @@
+import os
 import sys
 from pyboy import PyBoy
+from pathlib import Path, PosixPath, PurePath, PureWindowsPath
+
+SML_File = Path('ROMS', 'SML.gb')
+
+if not SML_File.exists:
+	print('You have to put a Super Mario Land ROM named "SML.gb" in ROMS/.')
+	exit(1)
 
 quiet = "--quiet" in sys.argv
-pyboy = PyBoy('ROMS/SML.gb', game_wrapper=True, window_type="headless" if quiet else "SDL2")
+pyboy = PyBoy(Path(SML_File).as_posix(), game_wrapper=True, window_type="headless" if quiet else "SDL2")
 pyboy.set_emulation_speed(0)
 sml = pyboy.game_wrapper()
 sml.start_game()
