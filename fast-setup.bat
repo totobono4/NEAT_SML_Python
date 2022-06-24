84569d7d281184391771aa5a4bb8dfed184101d3
theo.bonnaud <theo.bonnaud@imerir.com>
2022-06-24 09:43:43 +0200

    Test fast-setup

--- fast-setup.bat
+++ fast-setup.bat
@@ -1,6 +1,8 @@
+if not exist SML.gb EXIT /B
+move SML.gb Emulation/ROMS/
 python -m ensurepip --upgrade
 pip install -r requirements.txt
 cd Emulation/save_states_generator
 python save_state_generator.py ../ROMS/SML.gb
 cd ../neat
-python .\main.py ..\ROMS\SML.gb .\bests_configs\toto_10x10.txt ..\save_states_generator\saves_states\save_state_1-1.save
+python .\main.py ..\ROMS\SML.gb .\bests_configs\toto_10x10.txt ..\save_states_generator\saves_states\save_state_1-1.save --Display
